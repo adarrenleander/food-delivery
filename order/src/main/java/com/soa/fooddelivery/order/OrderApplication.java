fe31ecc008be55eedd531e6aa156e17106a3ff14
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2023-03-20 23:21:56 +0100

    add folder structure in each service

---
+++ order/src/main/java/com/soa/fooddelivery/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.soa.fooddelivery.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
