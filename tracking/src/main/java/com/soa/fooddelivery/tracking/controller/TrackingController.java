fa52be8d026f673b486812cb7d5815ddc307f6c9
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2023-04-09 14:57:54 +0200

    modify tracking service

--- tracking/src/main/java/com/soa/fooddelivery/tracking/controller/TrackingController.java
+++ tracking/src/main/java/com/soa/fooddelivery/tracking/controller/TrackingController.java
@@ -1,17 +1,21 @@
 package com.soa.fooddelivery.tracking.controller;
 
 import com.soa.fooddelivery.tracking.dto.OrderTrackingDto;
+import com.soa.fooddelivery.tracking.service.TrackingService;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TrackingController {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(TrackingController.class);
+    @Autowired private TrackingService trackingService;
 
     @GetMapping("/dispatch/{dispatchId}/track")
     public ResponseEntity<OrderTrackingDto> getOrderTracking(@PathVariable(name = "dispatchId") String dispatchId) {
-        OrderTrackingDto response = new OrderTrackingDto();
-        response.setDispatchId(dispatchId);
-        response.setDriverId("xxx");
+        log.debug("GET /dispatch/{dispatchId}/track getOrderTracking");
+        OrderTrackingDto response = trackingService.getOrderTracking(dispatchId);
         return ResponseEntity.ok().body(response);
     }
 }
