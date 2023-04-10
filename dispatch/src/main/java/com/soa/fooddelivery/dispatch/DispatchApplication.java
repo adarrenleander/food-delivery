1adbe88564e2d7a0039caad162d654be634244b8
Haraito <juwitaharaito@gmail.com>
2023-04-10 12:10:40 +0200

    Dispatch repository annotation fixed

--- dispatch/src/main/java/com/soa/fooddelivery/dispatch/DispatchApplication.java
+++ dispatch/src/main/java/com/soa/fooddelivery/dispatch/DispatchApplication.java
@@ -3,7 +3,7 @@ package com.soa.fooddelivery.dispatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"controller","service","entity", "repository"})
 public class DispatchApplication {
 
 	public static void main(String[] args) {
